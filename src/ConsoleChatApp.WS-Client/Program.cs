88b8c5375834e041894ec49c37dabf49308eb1fa
tdayko <ryan.bromati@hotmail.com>
2024-09-07 18:42:35 -0300

    feat: add users hanling on chat

--- src/ConsoleChatApp.WS-Client/Program.cs
+++ src/ConsoleChatApp.WS-Client/Program.cs
@@ -1,9 +1,32 @@
 ﻿using System.Net.WebSockets;
 using System.Text;
 
+string name = string.Empty;
+while (true)
+{
+    if (!string.IsNullOrWhiteSpace(name)) break;
+
+    Console.Write("Input your name: ");
+    name = Console.ReadLine()!.Trim();
+}
+
 var ws = new ClientWebSocket();
-await ws.ConnectAsync(new Uri("ws://localhost:6969/ws"), CancellationToken.None);
+await ws.ConnectAsync(new Uri($"ws://localhost:6969/ws?name={name}"), CancellationToken.None);
 Console.WriteLine("connected");
+
+var sendTask = Task.Run(async () =>
+{
+    while (true)
+    {
+        var message = Console.ReadLine();
+
+        if (string.IsNullOrWhiteSpace(message)) continue;
+        if (message == "/exit") break;
+
+        var bytes = Encoding.UTF8.GetBytes(message!);
+        await ws.SendAsync(new ArraySegment<byte>(bytes), WebSocketMessageType.Text, true, CancellationToken.None);
+    }
+});
 
 var receiveTask = Task.Run(async () =>
 {
@@ -14,8 +37,15 @@ var receiveTask = Task.Run(async () =>
         if (result.MessageType == WebSocketMessageType.Close) break;
 
         var message = Encoding.UTF8.GetString(buffer, 0, result.Count);
-        Console.WriteLine($"received: {message}");
+        Console.WriteLine($"{message}");
     }
 });
 
-await receiveTask;
+await Task.WhenAny(sendTask, receiveTask);
+
+if (ws.State != WebSocketState.Closed)
+{
+    await ws.CloseAsync(WebSocketCloseStatus.NormalClosure, "closing", CancellationToken.None);
+}
+
+await Task.WhenAll(sendTask, receiveTask);
